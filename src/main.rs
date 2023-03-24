a9777c42ecc0dcbcce032f0cf0e95261f4dba876
imrany <imranmat254@gmail.com>
2023-03-24 13:09:44 +0300

    if then else statement

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,12 @@ pub extern "C" fn add_one(x: i32) -> i32 {
 
 fn main() {
     println!("Hello, world! :{}",add_one(6));
+
+    //if then else
+    let n:i32=19;
+    if n>18{
+        println!("above 18");
+    }else{
+        println!("Below 18");
+    }
 }
