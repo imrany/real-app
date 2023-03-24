04b25cc690a3bacb0e2195b8f27af1b9277a9ac1
imrany <imranmat254@gmail.com>
2023-03-24 16:34:02 +0300

    testing a rust app

--- src/main.rs
+++ src/main.rs
@@ -15,3 +15,26 @@ fn main() {
         println!("Below 18");
     }
 }
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);//  assert! macro, verifying that neither argument is zero
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+            m = m % n;
+    }
+    n
+}
+
+#[test] //#[test] atop the definition marks test_gcd as a test function, to be skipped in normal compilations
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
+    3 * 7 * 11 * 13 * 19),
+    3 * 11);
+}
+//to run the test function run
+//cargo test
