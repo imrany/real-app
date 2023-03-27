90de55638e159c20500cda952f16b2830be71180
imrany <imranmat254@gmail.com>
2023-03-27 10:21:16 +0300

    resolving errors

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,68 @@
+mod config;
+mod handler;
+mod jwt_auth;
+mod model;
+mod response;
+
+use actix_cors::Cors;
 use actix_web::middleware::Logger;
-use actix_web::{
-    get,
-    App,
-    HttpResponse,
-    HttpServer,
-    Responder
-};
+use actix_web::{http::header, web, App, HttpServer};
+use config::Config;
+use dotenv::dotenv;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
-#[get("/api/healthchecker")]
- fn health_checker_handle()->impl Responder{
-    const MESSAGE: &str= "JWT Authentication in Rust using Actix-web, Postgres, and SQLX";
-
-    HttpResponse::Ok().json(serde_json::json!({"status":"success","message":MESSAGE}))
+pub struct AppState {
+    db: Pool<Postgres>,
+    env: Config,
 }
 
 // #[actix_web::main]
-fn main()->std::io::Result<()>{
-    if std::env::var_os("RUST_LOG").is_none(){
-        std::env::set_var("RUST_LOG","actix-web=info");
+fn main() -> std::io::Result<()> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_web=info");
     }
-    
-    // let port=std::env::var("PORT")||8080;
-    let port=8080;
-
+    dotenv().ok();
     env_logger::init();
-    println!("Server running on port {}",port);
+
+    let config = Config::init(); 
+
+    let pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&config.database_url)
+        // .await
+    {
+        Ok(pool) => {
+            println!("✅Connection to the database is successful!");
+            pool
+        }
+        Err(err) => {
+            println!("🔥 Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
+
+    println!("🚀 Server started successfully");
+
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![
+                header::CONTENT_TYPE,
+                header::AUTHORIZATION,
+                header::ACCEPT,
+            ])
+            .supports_credentials();
         App::new()
-        .wrap(Logger::default())
-        .service(health_checker_handle)
-    })
-    .bind(("127.0.0.1",port))?
+            .app_data(web::Data::new(AppState {
+                db: pool.clone(),
+                env: config.clone(),
+            }))
+            .configure(handler::config)
+            .wrap(cors)
+            .wrap(Logger::default())
+    }) 
+    .bind(("127.0.0.1", 8000))?
     .run()
+    // .await
 }
