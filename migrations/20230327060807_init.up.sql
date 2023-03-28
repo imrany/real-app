653e1461302ea36c57fb4753d8ed51ce0c22ca24
imrany <imranmat254@gmail.com>
2023-03-28 16:05:32 +0300

    create users table

--- migrations/20230327060807_init.up.sql
+++ migrations/20230327060807_init.up.sql
@@ -5,3 +5,10 @@
     isbn varchar not null
  );
  create unique index book_isbn_idx on book (isbn);
+
+ create table users(
+   username varchar(100) not null,
+   email varchar(100) not null,
+   pass_word varchar(24) not null 
+ )
+ create unique index email_idx on users (email)
