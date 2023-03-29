3ea2ffe4e0202034c0510ca64e89e520093274d0
imrany <imranmat254@gmail.com>
2023-03-29 09:51:43 +0300

    new changes

--- src/func/user_routes.rs
+++ src/func/user_routes.rs
@@ -10,6 +10,11 @@ use actix_web::{
 use sqlx::{self};
 use crate::AppState;
 
+use argon2::{
+    password_hash::{rand_core::OsRng,PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2,
+};
+
 mod models;
 use models::{
     User,
@@ -56,17 +61,34 @@ pub async fn register_user(
     body:Json<CreateUser>,
     state:Data<AppState>,
 )->impl Responder{
+    // let exists=sqlx::query("SELECT 1 FROM users WHERE email=$1")
+    // .bind(body.email.to_string())
+    // .fetch_one(&state.db)
+    // .await;
+
+    // {
+    //     Ok(_)=>HttpResponse::Conflict().json("User with that email already exists"),
+    //     Err(err)=>HttpResponse::Conflict().json(&err.to_string())
+    // }
+
+    let salt=SaltString::generate(&mut OsRng);
+    let argon2=Argon2::default();
+    
+    let hashed_password=argon2.hash_password(body.pass_word.as_bytes(),&salt)
+    .expect("Error while hashing password")
+    .to_string();
+
     match sqlx::query_as::<_,User>(
         "INSERT INTO users (username, email ,pass_word) VALUES ($1, $2 ,$3) RETURNING username, email"
     )
     .bind(body.username.to_string())
-    .bind(body.email.to_string())
-    .bind(body.pass_word.to_string())
+    .bind(body.email.to_string().to_lowercase())
+    .bind(hashed_password)
     .fetch_one(&state.db)
     .await
     {
         Ok(created_user)=>HttpResponse::Ok().json(created_user),
-        Err(err)=>HttpResponse::InternalServerError().json(["Failed to registe user",&err.to_string()])
+        Err(err)=>HttpResponse::InternalServerError().json(["Failed to register user",&err.to_string()])
     }
 }
 
@@ -76,14 +98,24 @@ pub async fn login_user(
     state:Data<AppState>,
 )->impl Responder{
     match sqlx::query_as::<_,User>(
-        "SELECT username, email, pass_word FROM users WHERE email=$1 AND pass_word=$2"
+        "SELECT username, email, pass_word FROM users WHERE email=$1"
     )
     .bind(body.email.to_string())
     .bind(body.pass_word.to_string())
     .fetch_one(&state.db)
     .await
     {
-        Ok(login_user)=>HttpResponse::Ok().json(login_user),
+        Ok(login_user)=>{
+            let parsed_hash=PasswordHash::new(body.pass_word.to_string())?;
+            Argon2::default().verify_password(login_user.pass_word.as_bytes(), &parsed_hash).is_ok()
+            .map(false,|_| true);
+            
+            if parsed_hash{
+                HttpResponse::Ok().json(login_user)
+            }else{
+                HttpResponse::NotAutorized().json("Not authorized")
+            }
+        },
         Err(err)=>HttpResponse::InternalServerError().json(["Failed to login",&err.to_string()])
     }
 }
